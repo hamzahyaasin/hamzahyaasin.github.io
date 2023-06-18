010c4f90bdfd216278fad900a2590b25ffce1057
hamzahyaasin <125568111+hamzahyaasin@users.noreply.github.com>
2023-06-18 20:49:15 +0500

    Add files via upload

---
+++ java portfolio.js
@@ -0,0 +1,21 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Form submission
+  document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    // Perform form submission or validation here
+    // You can use AJAX or fetch to send form data to a server
+    // or display an alert message to indicate successful submission
+    alert('Form submitted successfully!');
+  });
+  
