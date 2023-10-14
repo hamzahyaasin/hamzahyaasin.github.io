45bd35ea0ddda4968f3b38221a2b33fb0750f2fb
hamzahyaasin <125568111+hamzahyaasin@users.noreply.github.com>
2023-10-14 22:32:49 +0500

    Update java portfolio.js

--- java portfolio.js
+++ java portfolio.js
@@ -1,39 +1,20 @@
-// Smooth scroll effect for navigation links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetSection.offsetTop - 50,
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
 
-// Form validation and submission
-const contactForm = document.getElementById('contact-form');
-const successMessage = document.getElementById('success-message');
+// Form submission
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-contactForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Simple email validation
-    const email = document.getElementById('email').value;
-    if (!isValidEmail(email)) {
-        alert('Please enter a valid email address.');
-        return;
-    }
-
-    // Form submission logic (you can customize this part, e.g., send data to server)
-    // For demonstration purposes, simply show a success message
-    successMessage.textContent = 'Message sent successfully!';
-    contactForm.reset();
+  // Perform form submission or validation here
+  // You can use AJAX or fetch to send form data to a server
+  // or display an alert message to indicate successful submission
+  alert('Form submitted successfully!');
 });
-
-function isValidEmail(email) {
-    // Very basic email validation, you can use a regex or a more sophisticated method
-    return /\S+@\S+\.\S+/.test(email);
-}
